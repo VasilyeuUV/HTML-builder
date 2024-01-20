1ef0f7a8693d45e56b6aa725922c6a5a9401aace
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-01-20 14:21:54 +0300

    refactor: 01-read-file - upd. index html

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,8 @@ import File from '../_UserClasses/file.js';
 import pathModule from 'path';
 import { fileURLToPath } from 'url';
 
-const path = pathModule.dirname(fileURLToPath(import.meta.url));
-const file = new File(path, '/text.txt');
+const file = new File(
+  pathModule.dirname(fileURLToPath(import.meta.url)), 
+  '/text.txt'
+  );
 // file.contentToConsole();
